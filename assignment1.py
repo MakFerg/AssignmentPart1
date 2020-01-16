034178ed47d39d339ecc936bce001cff032c4a94
Makeda <lilmakfergie@gmail.com>
2020-01-16 15:07:47 +0100

    Created .csv file using python code

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,24 @@
+#open json file
 import json
 
 with open('AssignmentPart1/SouthAfrica1990.json', encoding='utf-16') as file:
     data= json.load(file)
-print(data)
+
+
+#open json file as csv with keys wanted :
+
+sample = data[0]
+
+with open("SouthAfrica1990.csv", "w", encoding= 'utf8') as file:
+    headers = file.write('type_of_violence, best_estimate\n')
+    for entry in data:
+        file.write(f"{entry['type_of_violence']},{entry['best']}\n")
+
+    
+        
+
+
+
 
 
 
